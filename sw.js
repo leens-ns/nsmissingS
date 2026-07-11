a68c3a8ce3652b3f2f84eed51ff1b52018a804a0
SEOB <user@seobiui-MacBookPro.local>
2026-07-12 00:25:18 +0900

    Improve lost item app admin access and UX

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = "nsmissing-teacher-v2";
+const SHELL_FILES = [
+  "./",
+  "./nsibmistchr.html",
+  "./manifest.webmanifest",
+  "./app-icon.svg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(SHELL_FILES)).catch(() => undefined)
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
